f9e95e2e92f9fc8ec575d1d769056d1d46f2e4e7
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-04-16 15:21:56 +0300

    complete-components

---
+++ src/components/CardAboutBottom.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const CardAboutBottom =({name, location}) => {
+    return (
+        <div className="card-about-container-bottom">
+            <div className="card-about-bottom">
+                <div>Name</div>
+                <div>{name}</div>
+            </div>
+            <div className="card-about-bottom">
+                <div>Location</div>
+                <div>{location}</div>
+        </div>
+      </div>
+    );
+};
+
+export default CardAboutBottom;
